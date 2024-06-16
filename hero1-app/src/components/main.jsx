c36d941f258ed11ed9b66bfb1e086b34dd4b512d
siobhan <sr@saibhreas.com>
2024-06-16 15:55:20 -0400

    have image running. Began using fucntion to introduce omponent. It allows for hoisting the vidoe to top of calls

--- hero1-app/src/components/main.jsx
+++ hero1-app/src/components/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import videoBg from '../assets/video/videoBg.mp4';
+import videoBg from '../assets/video/purple_moves.mp4';
 
-const Main = () =>{
+function Main () {
   return (
-    <div>
+    <div className='main'>
       <video  src={videoBg} autoPlay loop muted id="videoBg"></video>
+        <div className='content'>
+          <h1>This is my HERO jargon </h1>
+          <p>Welcome to here </p>
+
+          </div>
     </div>
   )
 }
