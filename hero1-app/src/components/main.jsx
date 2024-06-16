304b5de838a79a00337dde24314ca9270749907e
siobhan <sr@saibhreas.com>
2024-06-16 16:01:53 -0400

    aded overlay to alter color slightly

--- hero1-app/src/components/main.jsx
+++ hero1-app/src/components/main.jsx
@@ -4,6 +4,9 @@ import videoBg from '../assets/video/purple_moves.mp4';
 function Main () {
   return (
     <div className='main'>
+      <div className="overlay">
+        
+      </div>
       <video  src={videoBg} autoPlay loop muted id="videoBg"></video>
         <div className='content'>
           <h1>This is my HERO jargon </h1>
